bf3072c0e5d1bb4001dafa085a7de86fd46b88d3
Michael Hillcox <hello.michaelhillcox@gmail.com>
2016-09-09 22:24:54 +0100

    Moved scripts to footer var

--- echelon/me.php
+++ echelon/me.php
@@ -11,9 +11,9 @@ require 'app/views/global/header.php';
 		<form action="actions/edit-me.php" method="post" id="edit-me">
 			<fieldset class="none">
 				<legend>Account Info</legend>
-				
+
 				<label class="uname">Username:</label>
-					<input type="text" name="uname" value="<?php echo $_SESSION['username']; ?>" disabled="disabled" />		
+					<input type="text" name="uname" value="<?php echo $_SESSION['username']; ?>" disabled="disabled" />
 				<label for="display">Display Name:</label><?php tooltip('A name shown to all users, a name used to identify you'); ?>
 					<input type="text" name="name" value="<?php echo $mem->name; ?>" id="display" tabindex="1" />
 				<label for="email">Email:</label><?php tooltip('A valid email address where Echelon can contact you'); ?>
@@ -22,29 +22,30 @@ require 'app/views/global/header.php';
 
 			<fieldset class="none">
 				<legend>Change your password</legend>
-				
+
 				<label for="change-pw">Change your password:</label>
 					<input type="checkbox" name="change-pw" id="change-pw" tabindex="3" /><?php tooltip('Do you want to change your Echelon password'); ?><br />
-					
+
 				<label for="pass1" class="block">New Password:</label>
 					<input type="password" name="pass1" id="pass1" value="" class="disable" disabled="disabled" tabindex="4" />
 				<label for="pass2" class="block" style="width: 230px;">New Password Again:</label>
 					<input type="password" name="pass2" id="pass2" value="" class="disable" disabled="disabled" tabindex="5" />
 			</fieldset>
-			
+
 			<br class="clear" />
-			
+
 			<fieldset class="none" id="prove">
 				<legend>Prove Identiy</legend>
-					
+
 				<label for="password">Your Current Password:</label>
 					<input type="password" name="password" id="password" value="" tabindex="5" />
 			</fieldset>
-			
+
 			<br class="clear" />
-			
+
 			<input id="edit-me-submit" type="submit" value="Edit Me" class="button">
 		</form>
 	</fieldset>
-	
-<?php require 'app/views/global/footer.php'; ?>
+
+<?php $customPageScripts = "<script src=\"{$path}app/assets/js/me.js\" charset=\"{$charset}\"></script>";
+require 'app/views/global/footer.php'; ?>
