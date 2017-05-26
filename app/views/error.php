c5d5f9f362ec2140ba676d8d11012874bf6ba49d
Michael Hillcox <hello.michaelhillcox@gmail.com>
2017-05-26 18:54:53 +0100

    Styled Fatal error page.

--- app/views/error.php
+++ app/views/error.php
@@ -1,3 +1,30 @@
 <?php
+	include_once __DIR__."/../config.php";
+?>
 
-	var_dump($_SESSION['fatal_error_message']);
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="icon" type="image/png" href="<?= PATH ?>app/assets/images/logo-dark.png" />
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Echelon Install Package</title>
+		<link rel="stylesheet" href="<?= PATH ?>app/assets/bootstrap/css/bootstrap.css">
+		<link rel="stylesheet" href="<?= PATH ?>app/assets/styles/error.css">
+	</head>
+
+	<body>
+		<main class="error-page">
+			<div class="error">
+				<h1>Fatal Error!</h1>
+				<p><?= $_SESSION['fatal_error_message'] ?></p>
+
+				<a class="btn btn-success" href="<?= PATH ?>">Find Home</a>
+				<a class="btn btn-primary" href="<?= PATH ?>">Go back</a>
+			</div>
+		</main>
+	</body>
+</html>
