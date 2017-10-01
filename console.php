baa3c6033eb25b68b2b7d0e63e360082fc561a71
Michael Hillcox <hello.michaelhillcox@gmail.com>
2017-10-01 19:06:06 +0100

    Added a CLI command to generate and add a unque id to your config if you've already created a config.

--- console.php
+++ console.php
@@ -16,11 +16,28 @@ Here you can:
 
     if( $input == "0" ) {
 
-        
+        $config = __DIR__."/app/config.php";
 
+        // Check if the file exists
+        if( !file_exists($config) )
+            die( "Looks like you need to install echelon. Go to yourdomain/install" );
+
+        include __DIR__."/app/config.php";
+        if( defined( 'UNIQUE_KEY' ) )
+            die( "Looks like you already have unique key. You should never attempt to regenerate this\r\n" );
+
+        $file = file_get_contents($config);
+
+        $echKey = hash( "sha256", SES_SALT.time().DBL_PASSWORD );
+        $file .= "\r\ndefine('UNIQUE_KEY', '$echKey');";
+
+        file_put_contents( $config, $file );
+        print "Your unique key has been generated and added to your config.\r\n";
     }
     else
         print "Bye";
+
+    print "Bye";
 
     function getInput() {
         $stdin = fopen('php://stdin', 'r');
