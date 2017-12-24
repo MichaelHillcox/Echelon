4e920b47d391371deda01c69126615807f40a270
Michael Hillcox <hello.michaelhillcox@gmail.com>
2017-12-24 18:08:07 +0000

    Removed the use of setup.php
    Refactored a lot of what setup was doing. Still needs some works.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,8 @@
 if (!empty($_SERVER["SCRIPT_FILENAME"]) && "bootstrap.php" == basename($_SERVER["SCRIPT_FILENAME"]))
     die ("Please do not load this page directly. Thanks!"); // do not edit
 
+// TODO: Refactor all of this
+
 // Enable Logs
 error_reporting(E_ALL ^ E_NOTICE); // show all errors but notices
 
@@ -16,6 +18,41 @@ require 'classes/LegacyDatabase.php'; // class to preform all DB related actions
 require 'classes/Sessions.php'; // class to deal with the management of sesssions
 require 'classes/LegacyMembers.php'; // class to preform all B3 DB related actions
 
+$dbl = LegacyDatabase::getInstance(); // start connection to the local Echelon DB
+
+// Setup the main core
+$this_page = cleanvar($_SERVER["PHP_SELF"]);
+$cookie_time = time()+60*60*24*31; // 31 days from now
+
+## setup the game var ##
+$game = 1;
+if($_REQUEST['game']) {
+    $game = cleanvar($_REQUEST['game']);
+    setcookie("game", $game, $cookie_time, PATH); // set the cookie to game value
+} elseif($_COOKIE['game']) {
+    $game = cleanvar($_COOKIE['game']);
+} else
+    setcookie("game", $game, $cookie_time, PATH); // set the cookie to game value
+
+
+settype($game, "integer");
+if(!$dbl->isActiveGame($game))
+    set_warning('Attempting to access an inactive game');
+
+// TODO: refactor
+// find the number of games from the config array
+$config = [ "cosmos" => $dbl->getSettings() ];
+$num_games = $config['cosmos']['num_games'];
+$site_name = $config['cosmos']['name'];
+$limit_rows = $config['cosmos']['limit_rows'];
+$allow_ie = $config['cosmos']['allow_ie'];
+$min_pw_len = $config['cosmos']['min_pw_len'];
+$https_enabled = $config['cosmos']['https'];
+$key_expire = $config['cosmos']['user_key_expire']; // This var says how long it takes for a user creation key to expire
+$tformat = $config['cosmos']['time_format'];
+$time_zone = $config['cosmos']['time_zone'];
+$charset = $config['cosmos']['charset'];
+
 // TODO: Remove this
 ## If SSL required die if not an ssl connection ##
 if($https_enabled == 1) :
@@ -25,9 +62,77 @@ if($https_enabled == 1) :
     }
 endif;
 
-$dbl = LegacyDatabase::getInstance(); // start connection to the local Echelon DB
+// define email constant
+define("EMAIL", $config['cosmos']['email']);
 
-require 'common/setup.php'; // class to preform all DB related actions
+## Time Zone Setup ##
+$time_zone = (empty($time_zone) ? 'Europe/London' : $time_zone);
+define("NO_TIME_ZONE", $time_zone == '');
+date_default_timezone_set($time_zone);
+
+// if $game is greater than num_games then game doesn't exist so send to error page with error and reset game to 1
+if($num_games == 0) {
+    $no_games = true;
+
+} elseif($game > $num_games) {
+    setcookie("game", 1, time()+$cookie_time, $path); // set the cookie to game value
+    set_error('That game doesn\'t exist');
+    if($page != 'error')
+        sendError();
+}
+
+## Get the games Information for the current game ##
+$config['game'] = $dbl->getGameInfo($game);
+
+## setup the plugins into an array
+if(!empty($config['game']['plugins'])) {
+    $config['game']['plugins'] = explode(",", $config['game']['plugins']);
+    $no_plugins_active = false;
+} else
+    $no_plugins_active = true;
+
+## Get and setup the servers information into the array ##
+$servers = $dbl->getServers($game);
+
+$config['game']['servers'] = array(); // create array
+
+## add server information to config array##
+$i = 1; // start counter ("i") at 1
+
+if(!empty($servers) && $page != 'banlist') :
+    foreach($servers as $server) : // loop thro the list of servers for current game
+        $config['game']['servers'][$i] = array();
+        $config['game']['servers'][$i]['name'] = $server['name'];
+        $config['game']['servers'][$i]['ip'] = $server['ip'];
+        $config['game']['servers'][$i]['pb_active'] = $server['pb_active'];
+        $config['game']['servers'][$i]['rcon_pass'] = $server['rcon_pass'];
+        $config['game']['servers'][$i]['rcon_ip'] = $server['rcon_ip'];
+        $config['game']['servers'][$i]['rcon_port'] = $server['rcon_port'];
+
+        $i++; // increment counter
+    endforeach;
+endif;
+
+if($config['game']['num_srvs'] > 1) :
+    define("MULTI_SERVER", true);
+    define("NO_SERVER", false);
+elseif($config['game']['num_srvs'] == 1) :
+    define("MULTI_SERVER", false);
+    define("NO_SERVER", false);
+else :	// equal to no servers
+    define("MULTI_SERVER", false);
+    define("NO_SERVER", true);
+endif;
+
+## Setup some handy easy to access information for the CURRENT GAME only ##
+$game_id = $config['game']['id'];
+$game_name = $config['game']['name'];
+$game_name_short = $config['game']['name_short'];
+$game_num_srvs = $config['game']['num_srvs'];
+$game_active = $config['game']['active'];
+
+## setup default page number so this doesn't have to be in every file ##
+$page_no = 0;
 
 ## fire up the Sessions ##
 $ses = new Session(); // create Session instance
@@ -56,7 +161,7 @@ if(!$no_plugins_active) : // if there are any registered plugins with this game
 
     require 'classes/Plugins.php'; // require the plugins base class
 
-    $plugins = new Plugins(NULL);
+    $plugins = new Plugins();
 
     foreach($config['game']['plugins'] as $plugin) : // foreach plugin there is
 
