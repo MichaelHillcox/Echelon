abfb3f3cf6995dd24bbcd7b90e3b2a67dc3d1e1c
Michael Hillcox <hello.michaelhillcox@gmail.com>
2017-12-24 18:44:06 +0000

    Added all the routes

--- index.php
+++ index.php
@@ -11,11 +11,27 @@ $pagination = false;
 // this is a test
 // A fucking awful custom router.
 $router = [
-    'home'          => 'dashboard.php',
-    'clients'       => 'clients.php',
-    'client'        => 'clientdetails.php',
-    'login'         => 'login.php',
-    'active'        => 'active.php'
+    'home'              => 'dashboard.php',
+    'clients'           => 'clients.php',
+    'client'            => 'clientdetails.php',
+    'login'             => 'login.php',
+    'active'            => 'active.php',
+    'bans'              => 'bans.php',
+    'ban-list'          => 'banlist.php',
+    'plugin'            => 'plugins.php',
+    'kicks'             => 'kicks.php',
+    'map'               => 'map.php',
+    'me'                => 'me.php',
+    'notice'            => 'notices.php',
+    'public-bans'       => 'pubbans.php',
+    'register'          => 'register.php',
+    'regulars'          => 'regular.php',
+    'site-admins'       => 'sa.php',
+    'settings'          => 'settings.php',
+    'game-settings'     => 'settings-games.php',
+    'server-settings'   => 'settings-server.php',
+    'error'             => 'error.php',
+    'admins'            => 'admins.php'
 ];
 
 if( !isset($_GET['v']) || empty($_GET['v']) ) {
