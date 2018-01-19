b873b03cc374d74f0d2452ef5c406dbaff66fb21
Michael Hillcox <hello.michaelhillcox@gmail.com>
2018-01-19 19:50:25 +0000

    Fixed sesson issue hopefully

--- app/views/error.php
+++ app/views/error.php
@@ -1,5 +1,10 @@
 <?php
 	include_once __DIR__."/../config.php";
+
+    require __DIR__.'/../classes/Sessions.php'; // class to deal with the management of sesssions
+
+    $ses = new Session(); // create Session instance
+    $ses->sesStart('echelon', 0, PATH); // start session (name 'echelon', 0 => session cookie, path is echelon path so no access allowed oustide echelon path is allowed)
 ?>
 
 <!DOCTYPE html>
